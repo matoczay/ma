bbc73d60f52a0a53cf1df0d9aee8351533182ac0
ubuntu <ubuntu@ubuntu.ubuntu>
2018-05-24 12:49:36 +0200

    ok

--- script3.sh
+++ script3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 username='ubuntu'
 sudo apt-get update
-sudo apt-get install nano git apt-transport-https curl -y
+sudo apt-get install nano git apt-transport-https -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
